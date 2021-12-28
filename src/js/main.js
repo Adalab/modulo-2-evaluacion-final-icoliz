309b417fb905fd9dbaf42a960fcaf79b1d606d01
Ico Lizhen <icolizhen@gmail.com>
2021-12-28 12:25:06 +0000

    add basic html structure

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('>> Ready :)');
+
+const input = document.querySelector('.js_input');
+const searchBtn = document.querySelector('.js_searchBtn');
+const resetBtn = document.querySelector('.js_resetBtn');
+const movieList = document.querySelector('.js_movieList');
+
+console.log(input);
